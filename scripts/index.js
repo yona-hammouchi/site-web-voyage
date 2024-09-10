0362e57050ecf4c0f9d4bf296a8fc2fe68f22d83
Yonahammouchi <yona.hammouchi@outlook.com>
2024-09-10 17:23:23 +0200

    save

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,11 @@
 let menu_burger = document.getElementById('btn_burger')
 let wrapper_burger_menu = document.getElementById('wrapper_burger_menu')
+let button = document.getElementById("alertButton");
 
 menu_burger.addEventListener('click', function () {
     wrapper_burger_menu.classList.toggle('change');
+});
+
+button.addEventListener("click", function () {
+    alert("Bouton cliqué !");
 });
